1c8cb6280968322f3018711fc8ab273968f017e2
Iblis Lin <e196819@hotmail.com>
2015-08-30 18:04:45 +0800

    Change indentation style to 2 space

--- _attachments/static/app.js
+++ _attachments/static/app.js
@@ -1,41 +1,41 @@
 (function(){
-	var app = angular.module('couchForm', [])
-	.config(['$interpolateProvider', '$httpProvider',
-	function($interpolateProvider, $httpProvider) {
-		// $interpolateProvider.startSymbol('[[')
-		// $interpolateProvider.endSymbol(']]')
+  var app = angular.module('couchForm', [])
+  .config(['$interpolateProvider', '$httpProvider',
+          function($interpolateProvider, $httpProvider){
+    // $interpolateProvider.startSymbol('[[')
+    // $interpolateProvider.endSymbol(']]')
 
-		// for django request
-		// $httpProvider.defaults.xsrfCookieName = 'csrftoken'
-		// $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken'
-		// $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest'
-	}])
+    // for django request
+    // $httpProvider.defaults.xsrfCookieName = 'csrftoken'
+    // $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken'
+    // $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest'
+  }])
 
-	app.controller('IndexController', ['$scope', '$http',
-	function($scope, $http) {
-		var self = this
+  app.controller('IndexController', ['$scope', '$http',
+                 function($scope, $http){
+    var self = this
 
-		$scope.preview = {
-			list: []
-		}
+    $scope.preview = {
+      list: []
+    }
 
-		$scope.create = {
-			new: {
-				text: function(){
-					$scope.preview.list.push({
-						tag: 'input',
-						type: 'text',
-						placeholder: 'Some text here',
-					})
-				},
-			},
-			btn: function(){
-				$scope.preview.list = []
-				$scope.content_url = 'create.html'
-			},
-		}
+    $scope.create = {
+      new: {
+        text: function(){
+          $scope.preview.list.push({
+            tag: 'input',
+            type: 'text',
+            placeholder: 'Some text here',
+          })
+        },
+      },
+      btn: function(){
+        $scope.preview.list = []
+        $scope.content_url = 'create.html'
+      },
+    }
 
-		$scope.list = function(){
-		}
-	}])
+    $scope.list = function(){
+    }
+  }])
 })()
