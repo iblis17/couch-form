a0fb103eb2975763aff4ee3e61cf796cda60bb21
Iblis Lin <e196819@hotmail.com>
2015-08-30 22:58:19 +0800

    Add enter trigger finishing edit

--- _attachments/static/app.js
+++ _attachments/static/app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('couchForm', [])
+  var app = angular.module('couchForm', ['angular-loading-bar'])
   .config(['$interpolateProvider', '$httpProvider',
           function($interpolateProvider, $httpProvider){
     // $interpolateProvider.startSymbol('[[')
@@ -25,6 +25,20 @@
       this.edit_label = false
     }
   }
+
+  app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+      element.bind("keydown keypress", function (event) {
+        if(event.which === 13) {
+          scope.$apply(function (){
+            scope.$eval(attrs.ngEnter);
+          })
+
+          event.preventDefault();
+        }
+      })
+    }
+  })
 
   app.controller('IndexController', ['$scope', '$http',
                  function($scope, $http){
